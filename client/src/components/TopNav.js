4185b93b6553bcc0462636946382f582d38138aa
dev global <raybriones729@gmail.com>
2023-08-12 17:12:20 -0500

    added onscreen listener so if user scrolls and website sense a number above zero, the menu bar ui will appear. Also figured out how to make the bar fade in and out.

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,49 +1,72 @@
-import React from 'react'
-import {AiOutlineLogout} from 'react-icons/ai'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
-
-const TopNav = () => {
-
-    const navLinks = [
-        {name: "Home", link: '/'}, {/*this will all display at the top like tab buttons*/},
-        {name: "Tv Show", link: '/tv'},
-        {name: "My List", link: '/mylist'},
-        {name: "Movies", link: '/movies'},
-
-    ]
+import React from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Logo from "./images/Petflix_Logo.png"; //changed from server image to local image
+//isScrolled now takes the information from netflix.js
+const TopNav = ({ isScrolled }) => {
+  const navLinks = [
+    { name: "Home", link: "/" },
+    {
+      /*this will all display at the top like tab buttons*/
+    },
+    { name: "Tv Show", link: "/tv" },
+    { name: "My List", link: "/mylist" },
+    { name: "Movies", link: "/movies" },
+  ];
 
   return (
     <NavContainer>
-        <div className='leftSide'>
-            <div className='logo'>
-              <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668265433/NetflixApp/2560px-Netflix_2015_logo.svg_rbicwl_knwp6f.png'  alt='logo'/>
-            </div>
-            <ul className='links'>
-                {/* name and link from line 9 */}
-                {
-                    navLinks.map(({name, link}) => {
-                        return(
-                        <li key={name}>
-                            <Link to={link}> {name}</Link>
-                        </li>
-                        )
-                    }) 
-                }
-            </ul>
+      <nav className={`${isScrolled ? "scrolled" : "notScroll"}`}>
+        <div className="leftSide">
+          <div className="logo">
+            <img src={Logo} alt="logo" />
+          </div>
+          <ul className="links">
+            {/* name and link from line 9 */}
+            {navLinks.map(({ name, link }) => {
+              return (
+                <li key={name}>
+                  <Link to={link}> {name}</Link>
+                </li>
+              );
+            })}
+          </ul>
         </div>
 
-        <div className='rightSide'>
-            <button>
-                <AiOutlineLogout/>
-            </button>
-
+        <div className="rightSide">
+          <button>
+            <AiOutlineLogout />
+          </button>
         </div>
+      </nav>
     </NavContainer>
-  )
-}
+  );
+};
 
 const NavContainer = styled.div`
-`
+    .notScroll{
+        display: flex;
+    }
+    .scrolled{
+        display: flex;
+        background-color: black; {/*when scroll is above zero this is triggered and will show a black dropdown bar UI*/}
+    }
+    nav{
+        position: sticky;
+        top: 0;
+        height: 6rem;
+        width: 100%;
+        justify-content: space-between;
+        position: fixed;
+        z-index: 2;
+        padding: 0.4rem;
+        align-items: center;
+        transition : 0.3s ease-in-out;
+        .leftSide{
+            
+        }
+    }
+`;
 
-export default TopNav
+export default TopNav;
--- client/src/pages/Netflix.js
+++ client/src/pages/Netflix.js
@@ -4,7 +4,7 @@ import TopNav from '../components/TopNav'
 const Netflix = () => {
 
   const [isScrolled, setIsScrolled] = useState(false)// the initial state of webpage is false since user has not moved up or down (greater than zero will set it to TRUE)
-
+//below is windows scroll listener
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false: true)
       return () => (window.onscroll = null)
@@ -12,7 +12,7 @@ const Netflix = () => {
   console.log(isScrolled)
   return (
     <div className='Hero'>
-      <TopNav/>
+      <TopNav isScrolled = {isScrolled}/>
       <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg' alt = 'hero image'/> {/*change this to ori and cook*/}
       <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg' alt = 'hero image'/> {/*change this to ori and cook*/}
       <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg' alt = 'hero image'/> {/*change this to ori and cook*/}
