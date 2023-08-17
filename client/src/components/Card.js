1cbfc5a83fa7fdb65c1222a98224dcb4389ad055
dev global <raybriones729@gmail.com>
2023-08-17 14:44:11 -0500

    improved styling for buttons

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -69,8 +69,92 @@ const Card = () => {
 };
 
 const CardContainer = styled.div`
-  /* max-width: 230px; */
+  max-width: 230px;
+  width: 230px;
+  height: 100%;
+  cursor: pointer;
+  position: relative;
   background-color: red;
+  img{
+    border-radius: 0.2rem;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+  }
+  .hover{
+    z-index: 99%;
+    height: max-content;
+    width: 20rem;
+    position: absolute;
+    top: -18v;
+    left: 0;
+    border-radius: 0.2rem;
+    border: 0.1rem solid gray;
+    background-color: #181818;
+    transition: 0.3s ease-out;
+    .image-video-wrapper{
+        position: relative;
+        height: 140px;
+        img{
+            width: 100%;
+            height: 150px;
+            object-fit: cover;
+            border-radius: 0.3rem;
+            top: 0;
+            z-index: 4;
+            position: absolute;
+        }
+        video{
+            width: 100%;
+            height: 150px;
+            object-fit: cover;
+            border-radius: 0.3rem;
+            top: 0;
+            z-index: 4;
+            position: absolute;
+        }
+
+    }
+    .info-container{
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        gap: 0.5rem;
+    }
+    .icons{
+        display: flex;
+        justify-content: space-between;
+        .controls{
+            display: flex;
+            gap: 0.5rem;
+        }
+
+    svg{
+        color: white;
+        border: 0.1rem solid white;
+        border-radius: 50%;
+        font-size: 1.5rem;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover{
+            color: #b8b8b8;
+        }
+    }
+    }
+    .genres{
+        display: flex;
+        ul{
+            display: flex;
+            gap: 1rem;
+            li{
+                padding-right: 0.7rem;
+                &:first-of-type{
+                    list-style-type:
+                }
+            }
+        }
+    }
+  }
 `;
 
 export default Card;
